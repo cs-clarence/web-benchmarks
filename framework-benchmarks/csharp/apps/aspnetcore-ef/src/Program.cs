b57cf78595c75187973bcab64342366748712f5c
Clarence Manuel <rencedm112@gmail.com>
2025-01-08 23:07:43 +0800

    feat: aspnetcore ef

---
+++ framework-benchmarks/csharp/apps/aspnetcore-ef/src/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+// Configure the database context with EF Core and a connection string
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+app.MapGet("/users", async (AppDbContext dbContext) =>
+{
+    var users = await dbContext.Users
+        .AsNoTracking()
+        .Select(user => new
+        {
+            user.UserName,
+            user.EmailAddress,
+            user.FirstName,
+            user.LastName
+        })
+        .ToListAsync();
+
+    return Results.Ok(users);
+});
+
+app.Run();
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var userBuilder = modelBuilder.Entity<User>();
+
+        userBuilder.ToTable("users");
+        userBuilder.Property(user => user.UserName).HasColumnName("user_name");
+        userBuilder.Property(user => user.EmailAddress)
+            .HasColumnName("email_address");
+        userBuilder.Property(user => user.FirstName)
+            .HasColumnName("first_name");
+        userBuilder.Property(user => user.LastName).HasColumnName("last_name");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string EmailAddress { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
